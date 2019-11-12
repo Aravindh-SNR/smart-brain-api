2c8669b2f13673999d8bf2e8571c306e0b7710f9
Aravindh-SNR <aravindhsnr@gmail.com>
2019-11-12 23:57:22 +0530

    Modified the signup endpoint logic to include a db transaction

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const knex = require('knex');
 
+//For hashing passwords
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
+
+//Query builder library to connect with postgres database
+const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
@@ -19,66 +22,100 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//getting all the users
+//Getting all the users
+//This endpoint is not currently being used in the front-end and is for future use
 app.get('/', (request, response) => {
-    db.select('*').from('users').then(data => {
+    //SELECT * FROM users
+    db.select('*').from('users')
+    .then(data => {
         response.json(data);
-    });
+    })
+    .catch(error => {
+        response.status(400).json('Error getting users.');
+    })
 });
 
 //Sign in
 app.post('/signin', (request, response) => {
     const {email, password} = request.body;
-
-    db.select('email', 'hash').from('login').where({email}).then(data => {
+    //SELECT email, hash FROM login WHERE email = email
+    db.select('email', 'hash').from('login').where({email})
+    .then(data => {
+        //SELECT * FROM users WHERE email = email
+        //if the password hashes match
         data.length && bcrypt.compareSync(password, data[0].hash) ?
-        db.select('*').from('users').where({email}).then(data => {
+        db.select('*').from('users').where({email})
+        .then(data => {
             response.json(data[0]);
+        })
+        .catch(error => {
+            response.status(400).json('Error signing in.');
         })
         :
         response.json('Incorrect email or password, please try again.');
+    })
+    .catch(error => {
+        response.status(400).json('Error signing in.');
     });
 });
 
 //Sign up
 app.post('/signup', (request, response) => {
     const {name, email, password} = request.body;
-    db('users').insert({
-        name,
-        email,
-        joined: new Date()
-    }, ['*']).then(data => {
-        bcrypt.hash(password, saltRounds, (error, hash) => {
-            db('login').insert({
+    //Using a transaction so that the insert query into login table will take effect
+    //only if the insert query into users table is successful
+    db.transaction(transaction => {
+        //INSERT INTO users (name, email, joined) VALUES (name, email, new Date())
+        db('users').insert({
+            name,
+            email,
+            joined: new Date()
+        }, ['*'])
+        .transacting(transaction)
+        .then(async data => {
+            //INSERT INTO login (email, hash) VALUES (email, hash provided by the bcrypt library)
+            await db('login').insert({
                 email,
-                hash
-            }).then(() => {
-                response.json(data[0]);
-            });
-        });
-    }).catch(error => {
-        error.constraint === 'users_email_key' &&
-        response.json('Sorry, an account with the email you entered already exists.');
+                hash: bcrypt.hashSync(password, SALT_ROUNDS)
+            })
+            .transacting(transaction);
+            response.json(data[0]);
+        })
+        .then(transaction.commit)
+        .catch(transaction.rollback);
+    })
+    .catch(error => {
+        error.constraint === 'users_email_key' ?
+        response.json('Sorry, an account with the email you entered already exists.')
+        :
+        response.status(400).json('Error signing up.');
     });
 })
 
 //Getting one user
+//This endpoint is not currently being used in the front-end and is for future use
 app.get('/profile/:id', (request, response) => {
-    db.select('*').from('users').where({id: Number(request.params.id)}).then(data => {
-        data.length ? response.json(data[0]) : response.json('User not found');
-    });
+    const {id} = request.params;
+    //SELECT * FROM users WHERE id = id
+    db.select('*').from('users').where({id})
+    .then(data => {
+        data.length ? response.json(data[0]) : response.json('User not found.');
+    })
+    .catch(error => {
+        response.status(400).json('Error getting user.');
+    })
 });
 
 //Updating the user's rank
 app.put('/image', (request, response) => {
     const {id, score} = request.body;
-    db.select('entries').from('users').where({id}).then(data => {
-        data.length ?
-        db('users').where({id}).update({entries: Number(data[0].entries) + score}, ['entries']).then(data => {
-            response.json(data[0].entries);
-        })
-        :
-        response.json('User not found');
+    //UPDATE users SET entries = entries + score WHERE id = id
+    db('users').where({id}).increment('entries', score).returning('entries')
+    .then(data => {
+        data.length ? response.json(data[0]) : response.json('User not found.');
+    })
+    .catch(error => {
+        response.status(400).json('Error updating face count.');
     });
 })
 
