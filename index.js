2ab88ee35431eda25dcd4065e4b259b8f057cf6f
Aravindh-SNR <aravindhsnr@gmail.com>
2019-11-14 15:31:55 +0530

    Connecting to heroku postgres database

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'Pbv@90211sql',
-      database : 'smart-brain'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
     }
 });
 
